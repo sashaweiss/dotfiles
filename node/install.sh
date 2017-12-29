531094e1ca6b64b536a764ad61d6447706682b5b
sashaweiss <sasha@sashaweiss.coffee>
2017-12-28 18:12:47 -0600

    Getting node to install right

--- node/install.sh
+++ node/install.sh
@@ -3,9 +3,10 @@ git clone https://github.com/creationix/nvm.git "$nvm_dir"
 cd "$nvm_dir"
 git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
 
-if ! [ -d ~/.nvm ]; then
-  mkdir ~/.nvm
-fi
+export NVM_DIR=~/.nvm
+source "$NVM_DIR/nvm.sh"
+
+nvm install node
 
 if test ! `which tsc`; then
   npm install -g typescript
