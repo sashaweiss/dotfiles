9d418d1244fdd002e7792a48e853c257a7c8a965
sashaweiss <sasha@sashaweiss.coffee>
2017-12-28 18:34:40 -0600

    Reorganize fzf

--- fzf/env.zsh
+++ fzf/env.zsh
@@ -1,6 +1,9 @@
-source ~/.fzf.zsh
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+if [ -f ~/.fzf.zsh ]; then
+  source ~/.fzf.zsh
+fi
 
 if test `which rg`; then
   export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
 fi
+
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
