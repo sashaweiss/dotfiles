0f95f817ca1ea2ae0c093c517ad49ecd8a7c7b66
sashaweiss <sasha@sashaweiss.coffee>
2017-12-28 18:44:40 -0600

    Autoinstall plugins for vim

--- vim/install.sh
+++ vim/install.sh
@@ -1,5 +1,3 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# The first time you open `vim`, enter the command `:PlugInstall` - that'll install all the plugins listed in [vimrc](./vimrc).
-
-# For the `vim-go` plugin, make sure to run `:GoInstallBinaries` on first install.
+vim +PlugInstall +GoInstallBinaries +qa
