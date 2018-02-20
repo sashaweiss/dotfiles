29f302b38a79d96a70b998ce3ebf4b461fbd3fc8
sashaweiss <sasha@sashaweiss.coffee>
2018-02-20 17:30:52 -0600

    Update prompt separator

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -4,6 +4,6 @@ source $DOTFILES/zsh/gitstatus/zshrc.sh
 
 PROMPT=''
 # PROMPT+='${(r:$(tput cols)::-:)}'
-PROMPT+='%B%F{cyan}%n %f%F{white}.%f %F{magenta}%m %f%F{white}.%f %F{green}%1~%f$(git_super_status)%f
+PROMPT+='%B%F{cyan}%n %f%F{white}::%f %F{magenta}%m %f%F{white}::%f %F{green}%1~%f$(git_super_status)%f
 '
 PROMPT+='%F{white}%(1j.(%j) .)>>>%b %F{white}'
