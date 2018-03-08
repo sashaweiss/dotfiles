7a6971a940f75ef8d53691f3c709dd24829cf2d3
sashaweiss <sasha@sashaweiss.coffee>
2018-03-08 15:26:05 -0600

    Setting brew and python paths

--- brew/path.zsh
+++ brew/path.zsh
@@ -1,7 +1,5 @@
 if linux && test `which brew`; then
   PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
-elif test `which brew`; then
-  PATH="/usr/local/bin:$PATH"
 fi
 
 export PATH
--- python/path.zsh
+++
@@ -1,3 +0,0 @@
-if ebon; then
-  export PYTHONPATH="/System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python:/Library/Python/2.7/site-packages:/usr/.local/lib/python2.7/site-packages:$PYTHONPATH"
-fi
