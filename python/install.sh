da3a81c751b26a5d719ea51b10d9ec8446e1748f
sashaweiss <sasha@sashaweiss.coffee>
2018-03-08 16:04:24 -0600

    Use pipenv

--- python/install.sh
+++ python/install.sh
@@ -1,7 +1,4 @@
+#!/bin/zsh
+
 brew install python
-
-curl https://bootstrap.pypa.io/get-pip.py > get-pip.py
-sudo python get-pip.py
-rm get-pip.py
-
-sudo pip install virtualenv
+sudo pip install pipenv
