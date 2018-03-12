99713e2bf0c8faf2c71b81952d6ce3adb8e18049
sashaweiss <sasha@sashaweiss.coffee>
2018-03-11 23:06:46 -0500

    Update prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,7 +1,6 @@
 source $DOTFILES/zsh/gitstatus/zshrc.sh
 
 PROMPT=''
-# PROMPT+='${(r:$(tput cols)::-:)}'
-PROMPT+='%B%F{cyan}%n %f%F{white}::%f %F{magenta}%m %f%F{white}::%f %F{green}%1~%f$(git_super_status)%f
+PROMPT+='%B%F{cyan}%n%f %F{black}at%f %F{magenta}%m%f %F{black}in%f %F{green}%1~%f$(git_super_status)%f
 '
-PROMPT+='%F{white}%(1j.(%j) .)>>>%b %F{white}'
+PROMPT+='%F{blue}%(1j.(%j) .)>>>%b %F{white}'
