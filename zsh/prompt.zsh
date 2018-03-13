691cf9e1c27f240b82d464467b69b30ad2f664b6
sashaweiss <sasha@sashaweiss.coffee>
2018-03-13 13:05:59 -0500

    Make background tasks bright white

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,4 +3,4 @@ source $DOTFILES/zsh/gitstatus/zshrc.sh
 PROMPT=''
 PROMPT+='%B%F{cyan}%n%f %F{black}at%f %F{magenta}%m%f %F{black}in%f %F{green}%1~%f$(git_super_status)%f
 '
-PROMPT+='%F{blue}%(1j.(%j) .)>>>%b %F{white}'
+PROMPT+='%F{white}%(1j.(%j) .)%f%F{blue}>>>%f%b%F{white} '
