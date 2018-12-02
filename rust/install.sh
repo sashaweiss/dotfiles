d926ef3b60bfee4002947d57eaa15b4e62a2de6d
sashaweiss <asashaweiss@gmail.com>
2018-12-02 10:16:28 -0800

    Install rustfmt the proper way

--- rust/install.sh
+++ rust/install.sh
@@ -1,8 +1,7 @@
 #!/bin/zsh
 
 curl https://sh.rustup.rs -sSf | sh -s -- --no-modify-path -y
-rustup component add rust-src
-cargo install rustfmt
+rustup component add rust-src rustfmt-preview
 
 rustup toolchain add nightly
 reload
