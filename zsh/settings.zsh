575cfd890a55d828acf16ddadc06267f69955e63
sashaweiss <asashaweiss@gmail.com>
2019-04-09 19:39:53 -0700

    Case insensitive tab completion

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -10,3 +10,6 @@ HISTSIZE=100000
 SAVEHIST=$HISTSIZE
 HISTFILE=~/.zsh_history
 setopt hist_ignore_all_dups hist_reduce_blanks share_history hist_verify
+
+# Case-insensitive
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
