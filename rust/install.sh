207fc96df1ae9221eab4af2eff59a617e729f2b8
sashaweiss <asashaweiss@gmail.com>
2019-11-09 21:57:33 -0800

    Use rust-analyzer as VSCode lang server for Rust

--- rust/install.sh
+++ rust/install.sh
@@ -4,3 +4,12 @@ curl https://sh.rustup.rs -sSf | sh -s -- --no-modify-path -y
 rustup toolchain add nightly
 rustup component add rust-src rustfmt rust-analysis
 rustup component add rust-src rustfmt rust-analysis --toolchain nightly
+
+if ! command -v ra_lsp_server > /dev/null; then
+  TARGET="$DOTFILES/target"
+
+  mkdir -P "$TARGET"
+  git clone https://github.com/rust-analyzer/rust-analyzer "$TARGET/rust-analyzer"
+  cd "$TARGET/rust-analyzer"
+  cargo xtask install
+fi
