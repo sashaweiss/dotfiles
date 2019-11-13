a9860ae90cd5082d84e61eb85383e264477d050c
sashaweiss <asashaweiss@gmail.com>
2019-11-12 21:15:01 -0800

    Use rust-analyzer in coc.vim and add extension installations for vscode

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env zsh
 
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+
+extensions=(
+  'dustinsanders.an-old-hope-theme-vscode'
+  'matklad.ra-lsp'
+  'ms-python.python'
+  'nonylene.dark-molokai-theme'
+  'rust-lang.rust'
+  'timonwong.shellcheck'
+  'vscodevim.vim'
+)
+
+for extension in "${extensions[@]}"; do
+  code --install-extension $extension
+done
