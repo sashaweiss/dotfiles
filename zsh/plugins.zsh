1deb70a64b7d895bd097efc068e11d05b6d3123f
sashaweiss <asashaweiss@gmail.com>
2019-11-23 19:13:51 -0800

    Add .tmux submodule

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -31,3 +31,11 @@ ZSH_AUTOSUGGEST_STRATEGY=(history)
 
 # ref - https://github.com/zsh-users/zsh-autosuggestions#key-bindings
 bindkey -M viins '^f' vi-forward-word # next word, how `vim` `w` would advance
+
+# delete backwards by vi-word, ref - https://unix.stackexchange.com/questions/250690/how-to-configure-ctrlw-as-delete-word-in-zsh
+vi-backward-delete-word() {
+    local WORDCHARS=${WORDCHARS/\//}
+    zle backward-delete-word
+}
+zle -N vi-backward-delete-word
+bindkey -M viins '^w' vi-backward-delete-word # delete a word, how `vim` `b` would retreat
