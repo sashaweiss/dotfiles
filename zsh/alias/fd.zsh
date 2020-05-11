05665066830f50593129d66bb42ebebf271d2b27
sashaweiss <asashaweiss@gmail.com>
2020-05-11 14:37:41 -0700

    Add hidden to fda and fdas

--- zsh/alias/fd.zsh
+++ zsh/alias/fd.zsh
@@ -1,6 +1,6 @@
 command -v fd > /dev/null && {
   alias fd='fd --ignore-case'
-  alias fda='fd --no-ignore'
+  alias fda='fd --no-ignore --hidden'
   alias fds='fd --case-sensitive'
-  alias fdas='fd --no-ignore --case-sensitive'
+  alias fdas='fd --no-ignore --hidden --case-sensitive'
 }
