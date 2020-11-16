17a01957d160411460d762fa3db9d93c24d4152e
sashaweiss <asashaweiss@gmail.com>
2020-11-15 16:16:28 -0800

    Nix on WSL is a lil different

--- zsh/nix.zsh
+++ zsh/nix.zsh
@@ -1,7 +1,11 @@
 #!/usr/bin/env zsh
 
 if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
- . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+  # Multi-user install
+  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+elif [ -e '$HOME/.nix-profile/etc/profile.d/nix.sh' ]; then
+  # Single-user install on WSL
+  . '$HOME/.nix-profile/etc/profile.d/nix.sh'
 fi
 
 export NIX_PATH="$NIX_PATH:$HOME/.nix-defexpr/channels"
