b880efc7833314e1df32823f34bfd6eeeb7b70b5
sashaweiss <asashaweiss@gmail.com>
2020-11-15 21:12:08 -0800

    Variable interpolation fix for WSL install

--- zsh/nix.zsh
+++ zsh/nix.zsh
@@ -1,11 +1,11 @@
 #!/usr/bin/env zsh
 
-if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+if [ -f "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh" ]; then
   # Multi-user install
-  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-elif [ -e '$HOME/.nix-profile/etc/profile.d/nix.sh' ]; then
+  source "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh"
+elif [ -f "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then
   # Single-user install on WSL
-  . '$HOME/.nix-profile/etc/profile.d/nix.sh'
+  source "$HOME/.nix-profile/etc/profile.d/nix.sh"
 fi
 
 export NIX_PATH="$NIX_PATH:$HOME/.nix-defexpr/channels"
