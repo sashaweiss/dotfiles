4d61b6959781f1e6d265d2c7f37b7fd623edfac5
sashaweiss <asashaweiss@gmail.com>
2021-02-09 12:24:48 -0800

    Add vivid for ls_colors

---
+++ zsh/ls_colors.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+if command -v vivid > /dev/null; then
+  export LS_COLORS="$(vivid generate molokai)"
+else
+  # See: https://geoff.greer.fm/lscolors
+  export LS_COLORS='di=1;34:ln=36:so=32:pi=33:ex=1;32:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
+fi
