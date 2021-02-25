0773f5a14f579b8a0c063472508cce984414c4df
sashaweiss <asashaweiss@gmail.com>
2021-02-25 13:33:04 -0800

    Add a wsl alias for git.exe

--- zsh/alias/wsl.zsh
+++ zsh/alias/wsl.zsh
@@ -1,8 +1,10 @@
 #!/usr/bin/env zsh
 #
 if is_wsl; then
-  alias open=explorer.exe
+  alias open="explorer.exe"
 
-  alias pbcopy=clip.exe
-  alias pbpaste=clip.exe
+  alias pbcopy="clip.exe"
+  alias pbpaste="clip.exe"
+
+  alias git="git.exe"
 fi
