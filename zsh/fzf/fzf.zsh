844f2eee2030788f65a511b3d635f0ad136c2695
sashaweiss <asashaweiss@gmail.com>
2021-02-28 15:03:31 -0800

    Fix fzf shortcuts not working when not in tmux

--- zsh/fzf/fzf.zsh
+++ zsh/fzf/fzf.zsh
@@ -8,7 +8,7 @@
 #   - https://github.com/junegunn/fzf/wiki/examples
 #   - https://medium.com/@sidneyliebrand/how-fzf-and-ripgrep-improved-my-workflow-61c7ca212861
 #   - http://owen.cymru/fzf-ripgrep-navigate-with-bash-faster-than-ever-before/
-# 
+#
 # - @nathanshelly
 
 # <<<< completion >>>>
