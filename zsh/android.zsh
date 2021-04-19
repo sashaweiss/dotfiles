d4a6e8766fa0c5638ad2544b03921f8a8bdf35b6
sashaweiss <asashaweiss@gmail.com>
2021-04-19 11:41:21 -0700

    If Android Studio installed, point JAVA_HOME to it

--- zsh/android.zsh
+++ zsh/android.zsh
@@ -7,3 +7,7 @@ if [[ -d "$HOME/Library/Android/sdk" ]]; then
   path=($path "$ANDROID_SDK_ROOT/platform-tools")
   path=($path "$ANDROID_SDK_ROOT/emulator")
 fi
+
+if [[ -d "/Applications/Android Studio.app" ]]; then
+  export JAVA_HOME='/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home'
+fi
