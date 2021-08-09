32128034d3e16119dfd937101297750dcd3e22bf
sashaweiss <asashaweiss@gmail.com>
2021-08-09 11:55:49 -0700

    Changes from setting up new machine

--- zsh/brew.zsh
+++ zsh/brew.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
-if is_mac && [ -d "/usr/local/bin" ]; then
-  path=("/usr/local/bin" $path)
+if is_mac && [ -d "/opt/homebrew/bin" ]; then
+  path=("/opt/homebrew/bin" $path)
 elif is_linux && [ -d "/home/linuxbrew/.linuxbrew/bin" ]; then
   path=("/home/linuxbrew/.linuxbrew/bin" $path)
 fi
