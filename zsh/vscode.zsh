12be9fa03e50af76c04ab1416d70980a843bfcc5
sashaweiss <asashaweiss@gmail.com>
2021-09-07 11:26:04 -0700

    Alias code on WSL

--- zsh/vscode.zsh
+++ zsh/vscode.zsh
@@ -2,4 +2,6 @@
 
 if is_mac && [[ -d "/Applications/Visual Studio Code.app" ]]; then
   path=($path "/Applications/Visual Studio Code.app/Contents/Resources/app/bin")
+elif is_wsl; then
+  alias code='cmd.exe /c code'
 fi
