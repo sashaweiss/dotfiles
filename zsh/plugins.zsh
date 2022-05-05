e4f77486c1f1f696affbddaab88096304819eaee
sashaweiss <asashaweiss@gmail.com>
2022-05-05 14:11:12 -0700

    Fix bug around new zinit location

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 ### Install zinit if not present
-if ! [ -d "$HOME/.zinit" ]; then
+if ! [ -d "$HOME/.local/share/zinit/zinit.git" ]; then
   bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 fi
 
