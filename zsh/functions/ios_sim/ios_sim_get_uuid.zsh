1768483f752394b844df412e16946ed78d5b8b51
sashaweiss <asashaweiss@gmail.com>
2022-07-11 15:36:40 -0700

    Tweak sim_uuid function

--- zsh/functions/ios_sim/ios_sim_get_uuid.zsh
+++ zsh/functions/ios_sim/ios_sim_get_uuid.zsh
@@ -1,28 +1,28 @@
 #! /usr/bin/env zsh
 
-# [Stolen from Nora Trapp](https://gist.github.com/Imperiopolis-Signal/2a99392ff026ec918d6328643b5292ce)
+# [Heavy inspiration from Nora Trapp](https://gist.github.com/Imperiopolis-Signal/2a99392ff026ec918d6328643b5292ce)
 
 ios_sim_get_uuid () {
-    IFS='\n'
-    local sim_names=("${(@f)$(xcrun simctl list | grep Booted | awk -F'[\(|\)]' '{ print $1 }')}")
-    local sim_uuids=("${(@f)$(xcrun simctl list | grep Booted | awk -F'[\(|\)]' '{ print $(NF-3) }')}")
-    unset IFS
+  IFS='\n'
+  local sim_names=("${(@f)$(xcrun simctl list devices | grep "^    " | sed 's/^    //')}")
+  local sim_uuids=("${(@f)$(xcrun simctl list | grep "^    " | sed 's/^    //')}")
+  unset IFS
 
-    if [ "${#sim_uuids[@]}" -eq 0 ]; then
-      echo "No sims found!"
-      return 1
-    else
-      echo "Booted Simulators: "
+  if [ "${#sim_uuids[@]}" -eq 0 ]; then
+    echo "No sims found!"
+    return 1
+  else
+    echo "Simulators: \n"
 
-      for ((i = 1; i <= $#sim_names; i++)); do
-        echo "$i. $(echo ${sim_names[$i]} '('${sim_uuids[$i]}')' | xargs)"
-      done
+    for ((i = 1; i <= $#sim_names; i++)); do
+      echo "$i. $(echo ${sim_names[$i]} | xargs)"
+    done
 
-      echo -n "\nWhich simulator? "
-      read number
+    echo -n "\nWhich simulator? "
+    read number
 
-      local selected_uuid=${sim_uuids[$number]}
-    fi
+    local selected_uuid=${sim_uuids[$number]}
+  fi
 
-    SELECTED_IOS_SIM_UUID="$selected_uuid"
+  SELECTED_IOS_SIM_UUID="$selected_uuid"
 }
