1aaa47de1decd1823b943e5d13ba758d9bf47b3e
sashaweiss <asashaweiss@gmail.com>
2022-07-21 11:42:57 -0500

    Add copy function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -7,6 +7,7 @@ source "$DOTFILES/zsh/functions/ios_sim/ios_sim_boot.zsh"
 source "$DOTFILES/zsh/functions/ios_sim/ios_sim_get_uuid.zsh"
 source "$DOTFILES/zsh/functions/ios_sim/ios_sim_go_to_dir.zsh"
 
+source "$DOTFILES/zsh/functions/copy.zsh"
 source "$DOTFILES/zsh/functions/egs.zsh"
 source "$DOTFILES/zsh/functions/is_in_git_repo.zsh"
 source "$DOTFILES/zsh/functions/is_linux.zsh"
