f8e3d4b78e53bf38e6c44ff278aaaba9bff9b305
sashaweiss <asashaweiss@gmail.com>
2023-07-25 10:38:40 -0700

    Remove spectrum_ls and dashes for fd-copy

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -7,7 +7,6 @@ source "$DOTFILES/zsh/functions/fd-copy.zsh"
 source "$DOTFILES/zsh/functions/is_linux.zsh"
 source "$DOTFILES/zsh/functions/is_mac.zsh"
 source "$DOTFILES/zsh/functions/manage_proxy.zsh"
-source "$DOTFILES/zsh/functions/spectrum_ls.zsh"
 
 source "$DOTFILES/zsh/functions/git/e_git.zsh"
 source "$DOTFILES/zsh/functions/git/is_in_git_repo.zsh"
@@ -15,9 +14,3 @@ source "$DOTFILES/zsh/functions/git/nuke_branch.zsh"
 
 source "$DOTFILES/zsh/functions/fzf/fzf_gc.zsh"
 source "$DOTFILES/zsh/functions/fzf/fzf_gr.zsh"
-
-source "$DOTFILES/zsh/functions/ios_sim/ios_sim_boot.zsh"
-source "$DOTFILES/zsh/functions/ios_sim/ios_sim_get_uuid.zsh"
-source "$DOTFILES/zsh/functions/ios_sim/ios_sim_go_to_dir.zsh"
-
-source "$DOTFILES/zsh/functions/signal/launch_signal_on_sim.zsh"
--- zsh/functions/spectrum_ls.zsh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env zsh
-
-spectrum_ls () {
-  for i in {0..255}; do
-    print -Pn "%${i}F${(l:3::0:)i}%f " ${${(M)$((i%8)):#7}:+$'\n'};
-  done
-}
