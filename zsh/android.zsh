4b33f6a0759624d7c5f4120975cc7cde2aabae71
sashaweiss <asashaweiss@gmail.com>
2024-05-07 14:45:09 -0700

    Fix JAVA_HOME for new Android Studio

--- zsh/android.zsh
+++ zsh/android.zsh
@@ -9,5 +9,5 @@ if is_mac && [[ -d "$HOME/Library/Android/sdk" ]]; then
 fi
 
 if is_mac && [[ -d "/Applications/Android Studio.app" ]]; then
-  export JAVA_HOME='/Applications/Android Studio.app/Contents/jre/Contents/Home'
+  export JAVA_HOME='/Applications/Android Studio.app/Contents/jbr/Contents/Home'
 fi
