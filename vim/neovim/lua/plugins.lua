aab8e7ce873040d26bcfe61236ecded46dcc969b
sashaweiss <asashaweiss@gmail.com>
2024-06-27 14:23:39 -0700

    Switch to gitsigns

--- vim/neovim/lua/plugins.lua
+++ vim/neovim/lua/plugins.lua
@@ -20,8 +20,8 @@ Plug('scrooloose/nerdtree')
 Plug('junegunn/fzf')
 Plug('sashaweiss/fzf.vim', { ['branch'] = 'preview_fix' }) -- Use my fork which implements the community's solution to https://github.com/junegunn/fzf.vim/issues/751
 
--- Show line-based git-status in the gutter
-Plug('airblade/vim-gitgutter')
+-- Show git signs in the gutter
+Plug('lewis6991/gitsigns.nvim')
 
 -- Show a nice statusline at the bottom
 Plug('bling/vim-airline')
@@ -46,20 +46,54 @@ vim.call('plug#end')
 
 -- ========= Plugin Configuration =========
 
+-- gitsigns
+
+require('gitsigns').setup {
+	signs = {
+		add = { text = ' +' },
+		change = { text = ' ~' },
+		changedelete = { text = ' ~' },
+		delete = { text = ' -' },
+		topdelete = { text = ' -' },
+		untracked = { text = ' ?' },
+	},
+	signs_staged = {
+		add = { text = '|+' },
+		change = { text = '|~' },
+		changedelete = { text = '|~' },
+		delete = { text = '|-' },
+		topdelete = { text = '|-' },
+	},
+}
+
+vim.cmd.highlight({ "GitSignsAdd", "guifg=lightgreen", "guibg=none" })
+vim.cmd.highlight({ "GitSignsStagedAdd", "guifg=lightgreen", "guibg=none" })
+
+vim.cmd.highlight({ "GitSignsChange", "guifg=yellow", "guibg=none" })
+vim.cmd.highlight({ "GitSignsStagedChange", "guifg=yellow", "guibg=none" })
+
+vim.cmd.highlight({ "GitSignsChangedelete", "guifg=yellow", "guibg=none" })
+vim.cmd.highlight({ "GitSignsStagedChangedelete", "guifg=yellow", "guibg=none" })
+
+vim.cmd.highlight({ "GitSignsDelete", "guifg=red", "guibg=none" })
+vim.cmd.highlight({ "GitSignsStagedDelete", "guifg=red", "guibg=none" })
+
+vim.cmd.highlight({ "GitSignsTopdelete", "guifg=red", "guibg=none" })
+vim.cmd.highlight({ "GitSignsStagedTopdelete", "guifg=red", "guibg=none" })
+
+vim.cmd.highlight({ "GitSignsUntracked", "guifg=green", "guibg=none" })
+
 -- Airline
 
-vim.opt.laststatus = 2
 vim.g.airline_theme = 'base16_ashes'
 vim.g.airline_powerline_fonts = 1
 vim.g['airline#extensions#tabline#enabled'] = 1      -- Enable the list of buffers
 vim.g['airline#extensions#tabline#fnamemod'] = ':t'  -- Show just the filename
 
-
 -- NERDTree
 
 vim.keymap.set('n', '<leader>nt', ':NERDTreeToggle<CR>')
 vim.keymap.set('n', '<leader>nf', ':NERDTreeFocus<CR>')
-
 
 -- NERDCommenter
 
@@ -69,12 +103,10 @@ vim.g.NERDCustomDelimiters = { ['swift'] = { ['left'] = '//', ['right'] = '' } }
 
 vim.keymap.set('', '<leader>cc', '<plug>NERDCommenterInvert')
 
-
 -- Auto-Pairs
 
 vim.g.AutoPairsMultilineClose = 0
 vim.g.AutoPairs = { ['{'] = '}' }
-
 
 -- Subword Motion
 
