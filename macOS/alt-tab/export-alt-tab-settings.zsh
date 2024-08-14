2e4982d05319ef4fb0d18d061747efacba17b9d4
sashaweiss <asashaweiss@gmail.com>
2024-08-14 09:42:32 -0700

    Add Alt-Tab settings

---
+++ macOS/alt-tab/export-alt-tab-settings.zsh
@@ -0,0 +1,3 @@
+#!/usr/bin/env zsh
+
+defaults export "com.lwouis.alt-tab-macos" "$DOTFILES/macOS/alt-tab/com.lwouis.alt-tab-macos.plist"
---
+++ macOS/alt-tab/import-alt-tab-settings.zsh
@@ -0,0 +1,3 @@
+#!/usr/bin/env zsh
+
+defaults import "com.lwouis.alt-tab-macos" "$DOTFILES/macOS/alt-tab/com.lwouis.alt-tab-macos.plist"
