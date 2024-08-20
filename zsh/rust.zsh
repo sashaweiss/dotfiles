5ecf3d8667ac16546c1bfb0420b3bc2a83acf289
sashaweiss <asashaweiss@gmail.com>
2024-08-20 13:19:16 -0700

    Add comment about rust-toolchain being installed

--- zsh/rust.zsh
+++ zsh/rust.zsh
@@ -1,7 +1,12 @@
 #!/usr/bin/env zsh
 
 if command -v rustc > /dev/null; then
-  export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+  # Note that this relies on knowing which toolchain to use. That means that if
+  # this runs when a `rust-toolchain` file is present, and contains a toolchain
+  # we don't have installed, it'll be installed.
+  local rust_toolchain_root="$(rustc --print sysroot)"
+
+  export RUST_SRC_PATH="$rust_toolchain_root/lib/rustlib/src/rust/src"
 fi
 
 if [[ -d "$HOME/.cargo" ]]; then
