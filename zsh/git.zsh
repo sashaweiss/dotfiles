eb9048d9d09f24dc23d1da1f9e353481e412d4c2
sashaweiss <asashaweiss@gmail.com>
2024-09-10 11:01:00 -0700

    Add zsh completions for custom git commands

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,3 +1,6 @@
 #!/usr/bin/env zsh
 
-path=($path "$DOTFILES/util/git/custom-commands")
+GIT_COMMANDS_DIR="$DOTFILES/util/git/custom-commands"
+
+path=($path "$GIT_COMMANDS_DIR")
+fpath=($fpath "$GIT_COMMANDS_DIR/completions")
