edf53e9ddf48403dbcf8ce0c2de56254b1596675
sashaweiss <asashaweiss@gmail.com>
2025-02-06 13:43:31 -0800

    studio -> android alias

--- zsh/alias/android.zsh
+++ zsh/alias/android.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
 if is_mac && [ -d "/Applications/Android Studio.app" ]; then
-  alias studio='open -a /Applications/Android\ Studio.app'
+  alias android='open -a /Applications/Android\ Studio.app'
 fi
