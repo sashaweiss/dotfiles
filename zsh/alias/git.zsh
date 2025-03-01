00e9859bfbd03188c4114dae32c5c2ce8f6edab2
sashaweiss <asashaweiss@gmail.com>
2025-03-01 13:17:52 -0800

    Sort gb alias by committerdate, descending

--- zsh/alias/git.zsh
+++ zsh/alias/git.zsh
@@ -7,7 +7,7 @@ if command -v git > /dev/null; then
   alias gc='g commit'
   alias gd='g diff'
   alias gdc='gd --cached'
-  alias gb='g --no-pager branch'
+  alias gb='g --no-pager branch --sort=-committerdate'
   alias gba='gb -a'
   alias gco='g co'
 fi
