141ee15771b1495222785201187e112af8989082
sashaweiss <asashaweiss@gmail.com>
2025-08-20 19:05:15 -0700

    Use env vars during link creation

--- zsh/icloud.zsh
+++ zsh/icloud.zsh
@@ -1,5 +1,3 @@
 #!/usr/bin/env zsh
 
-export ICLOUD="$HOME/iCloud"
-
 alias icloud="cd $ICLOUD"
--- zsh/obsidian.zsh
+++ zsh/obsidian.zsh
@@ -1,5 +1,3 @@
 #!/usr/bin/env zsh
 
-export OBSIDIAN="$HOME/Library/Mobile Documents/iCloud~md~obsidian/Documents/Obsidian Notes"
-
 alias obsidian="cd '$OBSIDIAN'"
