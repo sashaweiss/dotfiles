59d7eaf1f0150b85062e57c7c6dacb95a49cc2d3
sashaweiss <asashaweiss@gmail.com>
2026-02-15 22:29:30 -0800

    Argument checking in cd-git-worktree

--- zsh/functions/cd-git-worktree.zsh
+++ zsh/functions/cd-git-worktree.zsh
@@ -4,5 +4,10 @@
 # a `git-foo`-style git custom command because
 # `cd` can't be run in a subshell.
 cd-git-worktree () {
-  cd "$@"
+  if [[ $# -ne 1 ]]; then
+    echo "Missing argument: <worktree-dir>" >&2
+    return 1
+  fi
+
+  cd "$1"
 }
