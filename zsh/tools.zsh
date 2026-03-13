299110be94d64f21c36d6aac279ab11c965c298c
sashaweiss <asashaweiss@gmail.com>
2026-03-13 12:01:53 -0700

    Adjust tools.zsh paths

--- zsh/tools.zsh
+++ zsh/tools.zsh
@@ -1,6 +1,4 @@
 #!/usr/bin/env zsh
 
-BUILT_FROM_SOURCE_BIN_DIR="$WORK/tools/bin"
-mkdir -p "$BUILT_FROM_SOURCE_BIN_DIR"
-
-path=($path "$BUILT_FROM_SOURCE_BIN_DIR")
+path=($path "$WORK/tools/bin")
+path=($path "$HOME/.local/bin")
