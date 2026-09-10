27523977e3eea580d52dfafa84ffec551aa98471
sashaweiss <asashaweiss@gmail.com>
2026-09-10 08:10:10 -0700

    Migrate to dewey

--- macOS/sol/export-config.zsh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env zsh
-
-cp "$HOME/.config/sol/config.json" "$DOTFILES/macOS/sol/config.json"
--- macOS/sol/scripts/icloud.sh
+++
@@ -1,4 +0,0 @@
-# name: iCloud Drive
-# icon: ⛈️
-
-open "$HOME/Library/Mobile Documents/com~apple~CloudDocs"
